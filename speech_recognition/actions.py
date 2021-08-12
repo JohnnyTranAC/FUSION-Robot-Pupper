f49c81cda396e794fa3a2c726f19e797bf679d57
JohnnyTranAC <74635740+JohnnyTranAC@users.noreply.github.com>
2021-08-12 16:35:56 -0700

    Update actions.py

--- speech_recognition/actions.py
+++ speech_recognition/actions.py
@@ -7,7 +7,7 @@ import os, sys
 #kit = ServoKit(channels=8)
 
 ### Initialize standing
-# 1-4: Hips,   5-8: Knees,   9-12: Sideways Hips
+## 1-4: Hips,   5-8: Knees,   9-12: Sideways Hips
 #kit.servo[2].angle = 0      # front left
 #kit.servo[6].angle = 0
 #kit.servo[1].angle = 40     # front right
@@ -19,46 +19,52 @@ import os, sys
 #time.sleep(1)
 
 ### Command Types
-# 1 - standing    2 - sitting
-# 3 - walk        4 - run
-# 5 - bark        6 - shake
+## 1 - standing    2 - sitting
+## 3 - walk        4 - run
+## 5 - bark        6 - shake
 
 ### Voice command input
 def action(commandType, position):
-    if commandType == 1 and position == 2:
+    if commandType == 1:
         ### Sitting to standing
         #kit.servo[4].angle = 0
         #kit.servo[8].angle = 0
         #kit.servo[3].angle = 40
         #kit.servo[7].angle = 40
         print("Pupper is now standing up\n")
-        sleep(3)
+        sleep(1.5)
         
-    elif commandType == 2 and position == 1:
+    elif commandType == 2:
         ### Standing to sitting
         #kit.servo[4].angle = 40
         #kit.servo[8].angle = 40
         #kit.servo[3].angle = 0
         #kit.servo[7].angle = 0
         print("Pupper is now sitting down\n")
-        sleep(3)
+        sleep(1.5)
         
-    elif commandType == 3 and commandType == 4:
-        print("Pupper has not yet learned to walk or run\n")
-        sleep(3)
+    elif commandType == 3:
+        os.system('omxplayer -o local /home/pi/speech_recognition/sounds/pant_slow.wav')
+        print("Pupper has not yet learned to walk\n")
+        sleep(1.5)
+        
+    elif commandType == 4:
+        os.system('omxplayer -o local /home/pi/speech_recognition/sounds/pant_fast.wav')
+        print("Pupper has not yet learned to run\n")
+        sleep(1.5)
         
     elif commandType == 5:
         ### Barking
-        os.system('omxplayer -o local /home/pi/speech_recognition/bark.wav')
+        os.system('omxplayer -o local /home/pi/speech_recognition/sounds/bark.wav')
         print("Pupper is now barking\n")
-        sleep(3)
+        sleep(1.5)
         
-    elif commandType == 6 and position == 1:
+    elif commandType == 6:
         ### Shake paw
         #kit.servo[1].angle = 120
         #kit.servo[5].angle = 0
         print("Pupper is now shaking its paw\n")
-        sleep(3)
+        sleep(1.5)
         
     else:
         print("ERROR: Pupper does not understand or cannot that action\n")
