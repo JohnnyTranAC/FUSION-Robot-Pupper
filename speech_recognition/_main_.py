f081ac284044f10b3386bcb6a1fdadb6a6bb1a31
JohnnyTranAC <74635740+JohnnyTranAC@users.noreply.github.com>
2021-08-12 16:34:00 -0700

    Update _main_.py

--- speech_recognition/_main_.py
+++ speech_recognition/_main_.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
+import os, sys
 from actions import action
+from conversation import *
 
 r = sr.Recognizer()
 m = sr.Microphone()
@@ -16,7 +18,15 @@ try:
         try:
             # recognize speech using Google Speech Recognition
             transcribe = r.recognize_google(audio)
-            commandList = ['Stand', 'Sit', 'Walk', 'Run', 'Bark', 'Shake', 'stand', 'sit', 'walk', 'run', 'bark', 'shake', 'goodnight']
+            convoSub_Starter = ['want to play outside', 'want to play inside', 'what time is it']
+            convoSub_Cont = ['how come', 'why not', 'I don\'t think', 'okay', 'what do you want to do']
+            commandList = ['Stand', 'Sit', 'Walk', 'Run', 'Bark', 'Shake', 'Sleep', 'stand', 'sit', 'walk', 'run', 'bark', 'shake', 'sleep', 'goodnight']
+            
+            if any(x in transcribe for x in convoSub_Starter):
+                convoStarter(transcribe)
+            
+            if any(x in transcribe for x in convoSub_Cont):
+                convoContinue(transcribe)
 
             if any(x in transcribe for x in commandList):
                 if transcribe.find('Stand')!=-1 or transcribe.find('stand')!=-1:
@@ -42,14 +52,19 @@ try:
                     print("Pupper will shake")
                     action(6, position)
                     position = 1
+                elif transcribe.find('Sleep')!=-1 or transcribe.find('sleep')!=-1:
+                    os.system('omxplayer -o local /home/pi/speech_recognition/sounds/snore.wav')
+                    print("Pupper is sleeping. Give him a few seconds")
+                    action(1, position)
+                    
                 elif transcribe.find('goodnight')!=-1:
                     print("Pupper is going to sleep. Goodnight")
                     break
             # we need some special handling here to correctly print unicode characters to standard output
             if str is bytes:  # this version of Python uses bytes for strings (Python 2)
-                print(u"You said '{}'".format(transcribe).encode("utf-8"))
+                print(u"You said '{}'\n".format(transcribe).encode("utf-8"))
             else:  # this version of Python uses unicode for strings (Python 3+)
-                print("You said '{}'".format(transcribe))
+                print("You said '{}'\n".format(transcribe))
         except sr.UnknownValueError:
             print("Oops! Didn't catch that")
         except sr.RequestError as e:
